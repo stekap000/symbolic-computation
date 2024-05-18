f466e251d6a09a38e2a293712a49ab8a435e5ebe
stekap000 <stekap000@gmail.com>
2024-05-18 20:02:44 +0200

    todo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+// TODO: Implement basic field symbolic algebra.
+// TODO: Implement basic symbolic derivation and integration.
+// TODO: Allow arbitrary axiomatic rules.
+
 int main() {
 	std::cout << "Setup complete" << std::endl;
 	
