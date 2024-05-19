85e404c87c3bb853d33bb2453f457583e50829df
stekap000 <stekap000@gmail.com>
2024-05-19 20:33:42 +0200

    todo

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 // TODO: Implement basic field symbolic algebra.
 // TODO: Implement basic symbolic derivation and integration.
 // TODO: Allow arbitrary axiomatic rules.
+// TODO: Introduce some kind of measure for expression compactness evaluation.
+// TODO: Heuristics for speeding up theorem search within formal system.
+// TODO: Axioms and rules specification via files.
+// TODO: Bind with latex at later stages.
 
 int main() {
 	std::cout << "Setup complete" << std::endl;
