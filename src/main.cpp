bf79012347032bb97c2183cae6ec4108724dbdab
stekap000 <stekap000@gmail.com>
2024-05-20 20:28:14 +0200

    internal representation init

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <cstdint>
+
+#include "common.h"
 
 // TODO: Implement basic field symbolic algebra.
 // TODO: Implement basic symbolic derivation and integration.
@@ -7,6 +12,35 @@
 // TODO: Heuristics for speeding up theorem search within formal system.
 // TODO: Axioms and rules specification via files.
 // TODO: Bind with latex at later stages.
+
+namespace repr {
+	enum Type {
+		num,
+		op
+		// ...
+	};
+	
+	struct Symbol {
+		Type type;
+		std::string name;
+	};
+
+	struct Integer {
+		std::vector<u64> blocks;
+
+		Integer() {
+			
+		}
+		
+		Integer from_string(std::string value_str) {
+			return Integer();
+		}
+	};
+
+	//struct Real {
+	//
+	//};
+}
 
 int main() {
 	std::cout << "Setup complete" << std::endl;
